01b7d86950f40bfd0bffb8f0a5beec0a3cac3b5f
molpie <molpie@tiscali.it>
2025-12-26 11:33:13 +0100

    Change THREE.js imports to local paths

--- js/viewer.js
+++ js/viewer.js
@@ -1,6 +1,6 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.162.0/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 // === Canvas e loading ===
 const canvas = document.getElementById("arch-viewer");
